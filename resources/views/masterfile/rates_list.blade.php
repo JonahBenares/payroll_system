c4614620a214391ca9f7e2efc121485bc43d20e4
stephinesev <stephineseverino.cenpri@gmail.com>
2022-10-24 14:14:11 +0800

    Add deduction type in adjustment rates

--- resources/views/masterfile/rates_list.blade.php
+++ resources/views/masterfile/rates_list.blade.php
@@ -67,6 +67,13 @@
                                                 <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Rate </label>
                                                 <input class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
                                             </div>
+                                            <div class="px-2 mt-2">
+                                                <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Deduction Type</label>
+                                                <select class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                    <option value="">Actual Amount</option>
+                                                    <option value="">Percentage</option>
+                                                </select>
+                                            </div>
                                             <div class="flex justify-end mt-6 px-2">
                                                 <button type="button" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl w-full white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
                                                     Save
@@ -172,6 +179,13 @@
                                                             <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Rate </label>
                                                             <input class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
                                                         </div>
+                                                        <div class="px-2 mt-2">
+                                                            <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Deduction Type</label>
+                                                            <select class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                                <option value="">Actual Amount</option>
+                                                                <option value="">Percentage</option>
+                                                            </select>
+                                                        </div>
                                                         <div class="flex justify-end mt-6 px-2">
                                                             <button type="button" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl w-full white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
                                                                 Update
@@ -253,6 +267,13 @@
                                                         <div class="px-2 mt-2">
                                                             <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Rate </label>
                                                             <input class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                        </div>
+                                                        <div class="px-2 mt-2">
+                                                            <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Deduction Type</label>
+                                                            <select class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                                <option value="">Actual Amount</option>
+                                                                <option value="">Percentage</option>
+                                                            </select>
                                                         </div>
                                                         <div class="flex justify-end mt-6 px-2">
                                                             <button type="button" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl w-full white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
@@ -336,6 +357,13 @@
                                                             <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Rate </label>
                                                             <input class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
                                                         </div>
+                                                        <div class="px-2 mt-2">
+                                                            <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Deduction Type</label>
+                                                            <select class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                                <option value="">Actual Amount</option>
+                                                                <option value="">Percentage</option>
+                                                            </select>
+                                                        </div>
                                                         <div class="flex justify-end mt-6 px-2">
                                                             <button type="button" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl w-full white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
                                                                 Update
@@ -418,6 +446,13 @@
                                                             <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Rate </label>
                                                             <input class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
                                                         </div>
+                                                        <div class="px-2 mt-2">
+                                                            <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Deduction Type</label>
+                                                            <select class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                                <option value="">Actual Amount</option>
+                                                                <option value="">Percentage</option>
+                                                            </select>
+                                                        </div>
                                                         <div class="flex justify-end mt-6 px-2">
                                                             <button type="button" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl w-full white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
                                                                 Update
@@ -499,6 +534,13 @@
                                                         <div class="px-2 mt-2">
                                                             <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Rate </label>
                                                             <input class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                        </div>
+                                                        <div class="px-2 mt-2">
+                                                            <label for="" class="block text-sm text-gray-700 capitalize white:text-gray-200">Deduction Type</label>
+                                                            <select class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                                <option value="">Actual Amount</option>
+                                                                <option value="">Percentage</option>
+                                                            </select>
                                                         </div>
                                                         <div class="flex justify-end mt-6 px-2">
                                                             <button type="button" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl w-full white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
