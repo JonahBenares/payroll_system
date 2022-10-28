20ea5ccad92328d43ad92dd14b9d31ac73bcbb55
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2022-10-28 15:18:23 +0800

    masterfile redo

--- storage/framework/views/64f2bf60829069728daa91f44cf470970e0e91bd.php
+++ storage/framework/views/64f2bf60829069728daa91f44cf470970e0e91bd.php
@@ -39,7 +39,7 @@
 					</div>
 				</div>
 				<?php if(Session::has('success')): ?>
-                    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                         <span class="block sm:inline"><?php echo e(Session::get('success')); ?></span>
                     </div>
                 <?php endif; ?>
