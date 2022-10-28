16c4ed5215e8ef53e1d1e1c3e6960b014e70c73b
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2022-10-28 13:22:44 +0800

    masterfile redo

--- storage/framework/views/c3bcd01c5df96df05edb5039fcf90a3f64805319.php
+++ storage/framework/views/c3bcd01c5df96df05edb5039fcf90a3f64805319.php
@@ -68,7 +68,7 @@
                                 </td>
                                 <td class="py-3 px-6">EOM</td>
                                 <td class="py-3 px-6 flex justify-center" align="center">
-                                    <a href="<?php echo e(route('deductions.edit','1')); ?>" class="" title="Update">
+                                    <a href="<?php echo e(route('deductions.edit' , '1')); ?>" class="" title="Update">
                                         <div class="py-2 px-2 text-xs font-medium text-center text-white transition-colors bg-indigo-500 rounded-2xl white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
                                                 <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
