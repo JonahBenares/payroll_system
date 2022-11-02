72055289c162f9c198fb67449838cd0dce9f4a2a
unknown <glennpaul.cenpri@gmail.com>
2022-11-02 11:49:25 +0800

    deduction schedule controller and index function

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,13 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.getElementById('sched_type').addEventListener("change", function (e) {
+    if (e.target.value === 'regular') {
+        document.getElementById('regulars').style.display = 'block';
+        document.getElementById('shiftings').style.display = 'none';
+    } else {
+        document.getElementById('regulars').style.display = 'none';
+        document.getElementById('shiftings').style.display = 'block'
+    }
+});
