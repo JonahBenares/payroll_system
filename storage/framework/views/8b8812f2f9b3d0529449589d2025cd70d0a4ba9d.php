7122f2fad97578762798aa93e56f42d9afa7ff68
stephinesev <stephineseverino.cenpri@gmail.com>
2022-11-02 09:59:37 +0800

    conflict

---
+++ storage/framework/views/8b8812f2f9b3d0529449589d2025cd70d0a4ba9d.php
@@ -0,0 +1,88 @@
+<?php if (isset($component)) { $__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da = $component; } ?>
+<?php $component = $__env->getContainer()->make(App\View\Components\AppLayout::class, [] + (isset($attributes) ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $constructor = (new ReflectionClass(App\View\Components\AppLayout::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+     <?php $__env->slot('header', null, []); ?>  <?php $__env->endSlot(); ?>
+    <!-- component -->
+    <style>
+        [x-cloak] {
+        display: none;
+        }
+    </style>
+    
+    <div class="overflow-auto h-screen pb-28 pt-2 pr-2 pl-2 md:pt-0 md:pr-0 md:pl-0">
+        <div class="flex flex-col flex-wrap sm:flex-row ">
+            <div class="p-4 relative h-full w-full text-center bg-white rounded-2xl shadow-lg white:bg-gray-800 white:border-gray-700">
+                <div class="flex justify-between  pb-4 bg-white white:bg-gray-900">
+                    <div > 
+                        <h2 class="uppercase font-semibold py-2">Allowance <small class="border-l-2 px-1">Update</small></h2>
+                    </div>
+                    <div class="flex">
+                        <a href="<?php echo e(route('allowance.index')); ?>"  class="flex items-center justify-center px-3 py-2 mx-2 space-x-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                            <span>Show List</span>
+                        </a>
+                    </div>
+                </div>
+                <?php if(Session::has('success')): ?>
+                    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                        <span class="block sm:inline"><?php echo e(Session::get('success')); ?></span>
+                    </div>
+                <?php endif; ?>
+                <?php if(Session::has('fail')): ?>
+                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                        <span class="block sm:inline"><?php echo e(Session::get('fail')); ?></span>
+                    </div>
+                <?php endif; ?>
+                <?php $__currentLoopData = $allowances; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $a): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <form class="mt-5" action="<?php echo e(route('allowance.update',$a->id)); ?>" method='POST'>
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('PUT'); ?>
+                    <div class="px-2">
+                        <label for="" class="block text-left text-sm text-gray-700 capitalize white:text-gray-200">Allowance Name</label>
+                        <input type="text" name="allowance_name" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" value="<?php echo e($a->allowance_name); ?>">
+                    </div>
+                    <div class="px-2 mt-2">
+                        <label for="" class="block text-left text-sm text-gray-700 capitalize white:text-gray-200">Default Rate</label>
+                        <input type="text" onkeypress="return isNumberKey(this, event)" name="allowance_rate" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" value="<?php echo e($a->allowance_rate); ?>">
+                        <input type="hidden" name="id" value="<?php echo e($a->id); ?>">
+                    </div>
+                    <div class="flex justify-end mt-6 px-2">
+                        <button type="submit" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl w-full white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                            Update
+                        </button>
+                    </div>
+                </form>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </div>
+            
+        </div>
+    </div>    
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da)): ?>
+<?php $component = $__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da; ?>
+<?php unset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da); ?>
+<?php endif; ?>
+<script>
+    function isNumberKey(txt, evt){
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode == 46) {
+            //Check if the text already contains the . character
+            if (txt.value.indexOf('.') === -1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (charCode > 31
+                && (charCode < 48 || charCode > 57))
+                return false;
+        }
+        return true;
+    }
+</script><?php /**PATH C:\xampp\htdocs\payroll_system\resources\views/allowances/allowance_update.blade.php ENDPATH**/ ?>
