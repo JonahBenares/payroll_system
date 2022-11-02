ed3dd04914a246b76ed0eb8d8d8fbf0b814004a1
unknown <glennpaul.cenpri@gmail.com>
2022-11-02 10:56:16 +0800

    statutory bracket masterfile functions

--- app/Http/Controllers/StatutoryBracketController.php
+++ app/Http/Controllers/StatutoryBracketController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\StatutoryBracket;
+use App\Models\PayslipInfo;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,10 @@ class StatutoryBracketController extends Controller
      */
     public function index()
     {
-        return view('statutory.index');
+        //$stat = StatutoryBracket::all();
+        $stat = StatutoryBracket::join('payslip_info', 'payslip_info.id', '=', 'statutory_bracket.payslip_info_id')
+        ->get(['statutory_bracket.id','payslip_info.description','statutory_bracket.salary_from','statutory_bracket.salary_to','statutory_bracket.deduction_amount','statutory_bracket.as_of']);
+        return view ('statutory.index')->with('statutory', $stat);
     }
 
     /**
@@ -25,7 +29,10 @@ class StatutoryBracketController extends Controller
      */
     public function create()
     {
-        return view('statutory.create');
+        //$statutory=PayslipInfo::all()->sortBy('description');
+        $statutory = PayslipInfo::where('pay_type',"=",'3')->get();
+        return view('statutory.create',compact('statutory'));
+        //return view('statutory.create');
     }
 
     /**
@@ -36,7 +43,18 @@ class StatutoryBracketController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $stat=new StatutoryBracket();
+        $stat->payslip_info_id=$request->payslip_info_id;
+        $stat->salary_from=$request->salary_from;
+        $stat->salary_to=$request->salary_to;
+        $stat->deduction_amount=$request->deduction_amount;
+        $stat->as_of=$request->as_of;
+        $res = $stat->save();
+        if($res){
+            return redirect()->route('statutorybracket.create')->with('success',"Statutory Bracket Added Successfully");
+        }else{
+            return redirect()->route('statutorybracket.create')->with('fail',"Error! Try Again!");
+        }
     }
 
     /**
@@ -45,9 +63,10 @@ class StatutoryBracketController extends Controller
      * @param  \App\Models\StatutoryBracket  $statutoryBracket
      * @return \Illuminate\Http\Response
      */
-    public function show(StatutoryBracket $statutoryBracket)
+    public function show($id)
     {
-        //
+        $stat = StatutoryBracket::find($id);
+        return view('statutory.show')->with('statutory', $stat);
     }
 
     /**
@@ -56,9 +75,11 @@ class StatutoryBracketController extends Controller
      * @param  \App\Models\StatutoryBracket  $statutoryBracket
      * @return \Illuminate\Http\Response
      */
-    public function edit(StatutoryBracket $statutoryBracket)
+    public function edit($id)
     {
-        return view('statutory.edit');
+        $stat = StatutoryBracket::find($id);
+        $pay = PayslipInfo::where('pay_type',"=",'3')->get();
+        return view('statutory.edit')->with('statutory', $stat)->with('payslip', $pay);
     }
 
     /**
@@ -68,9 +89,12 @@ class StatutoryBracketController extends Controller
      * @param  \App\Models\StatutoryBracket  $statutoryBracket
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, StatutoryBracket $statutoryBracket)
+    public function update(Request $request, $id)
     {
-        //
+        $stat = StatutoryBracket::find($id);
+        $input = $request->all();
+        $stat->update($input);
+        return redirect()->route('statutorybracket.edit',$id)->with('success',"Statutory Bracket Updated Successfully");
     }
 
     /**
@@ -79,8 +103,9 @@ class StatutoryBracketController extends Controller
      * @param  \App\Models\StatutoryBracket  $statutoryBracket
      * @return \Illuminate\Http\Response
      */
-    public function destroy(StatutoryBracket $statutoryBracket)
+    public function destroy($id)
     {
-        //
+        StatutoryBracket::destroy($id);
+        return redirect()->route('statutorybracket.index')->with('success',"Statutory Bracket Deleted Successfully");
     }
 }
--- app/Models/StatutoryBracket.php
+++ app/Models/StatutoryBracket.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class StatutoryBracket extends Model
 {
     use HasFactory;
+    protected $table = 'statutory_bracket';
+    protected $primaryKey = 'id';
+    protected $fillable = ['payslip_info_id', 'salary_from', 'salary_to', 'deduction_amount', 'as_of'];
 }
