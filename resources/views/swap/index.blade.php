0cf47769fe8c182d21aaa569a6112a46ac0723e5
unknown <jonahbenares.cenpri@gmail.com>
2022-11-17 14:07:36 +0800

    conflict

--- resources/views/swap/index.blade.php
+++ resources/views/swap/index.blade.php
@@ -1,11 +1,6 @@
 <x-app-layout>
     <x-slot name="header"></x-slot>
     <!-- component -->
-    <style>
-        [x-cloak] {
-        display: none;
-        }
-    </style>
     
     <div class="overflow-auto h-screen pb-28 pt-2 pr-2 pl-2 md:pt-0 md:pr-0 md:pl-0">
         <div class="flex flex-col flex-wrap sm:flex-row ">
@@ -77,25 +72,144 @@
                 </div>
                 </form>
                 <div class="overflow-x-auto overflow-y-hidden hover:overflow-y-auto h-100 relative max-h-100 pt-2 pr-2 pl-2 mt-3 md:pt-0 md:pr-0 md:pl-0 sm:rounded-2xl">
-                    <table class="w-full text-sm text-left text-gray-500 white:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 white:bg-gray-700 white:text-gray-400 sticky top-0 z-10">
+                    <table class=" " width="110%">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
                             <tr class="">
-                                <th scope="col" class="py-3 px-6" width="20%">
+                                <th class="text-left px-2 py-3 sticky left-0  bg-gray-50" width="8%">
                                     Employee Name
                                 </th>
-                                <th scope="col" class="py-3 px-6" width="10%">
+                                <th class="text-left px-2 py-3" width="6%">
                                     Filed Date
                                 </th>
-                                <th scope="col" class="py-3 px-6" width="15%">
+                                <th class="text-left px-2 py-3" width="6%">
                                     Shift From (RD)
                                 </th>
-                                <th scope="col" class="py-3 px-6" width="15%">
+                                <th class="text-left px-2 py-3" width="6%">
                                     Shift To (Duty)
                                 </th>
-                                <th scope="col" class="py-3 px-6" width="15%">
+                                <th class="text-left px-2 py-3" width="6%">
                                     Shift From (Duty)
                                 </th>
-                                <th scope="col" class="py-3 px-6" width="15%">
+                                <th class="text-left px-2 py-3" width="6%">
+                                    Shift To (RD)
+                                </th>
+                                <th scope="col" class="px-2 py-3 sticky right-0  bg-gray-50" width="1%" align="center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                    </svg>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($swapdata AS $sd)
+                            <tr class=" border-b white:bg-gray-800 white:border-gray-700 hover:bg-gray-50 white:hover:bg-gray-600">
+                                <td class="text-sm text-left px-2 py-3 sticky left-0 bg-white">
+                                    {{ $sd->full_name }}
+                                </td>
+                                <td class="text-sm text-left px-2 py-3">
+                                    {{ $sd->file_date }}
+                                </td>
+                                <td class="text-sm text-left px-2 py-3">
+                                    {{ $sd->shift_from_rd }}
+                                </td>
+                                <td class="text-sm text-left px-2 py-3">
+                                    {{ $sd->shift_to_duty }}
+                                </td>
+                                <td class="text-sm text-left px-2 py-3">
+                                    {{ $sd->shift_from_duty }}
+                                </td>
+                                <td class="text-sm text-left px-2 py-3">
+                                    {{ $sd->shift_to_rd }}
+                                </td>
+                                <td class="px-2 py-3 sticky right-0 bg-white justify-center flex space-x-2">
+                                    <a href="{{ route('swapschedule.edit',$sd) }}" class="" title="Update">
+                                        <div class="py-2 px-2 text-xs font-medium text-center text-white transition-colors bg-indigo-500 rounded-2xl white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                                <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                                                <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                                            </svg>
+                                        </div>
+                                    </a>
+                                    <div x-data="{ modelOpen: false }">
+                                        <a href="#" @click="modelOpen =!modelOpen" class="" title="Cancel">
+                                            <div class="py-2 px-2 text-xs font-medium text-center text-white bg-red-500 rounded-2xl hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-blue-300 white:bg-blue-600 white:hover:bg-red-500 white:focus:ring-blue-800">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                                </svg> 
+                                            </div>
+                                        </a>
+                                
+                                        <div x-show="modelOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                                            <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+                                                <div x-cloak @click="modelOpen = false"  
+                                                    x-transition:enter="transition ease-out duration-300 transform"
+                                                    x-transition:enter-start="opacity-0" 
+                                                    x-transition:enter-end="opacity-100"
+                                                    x-transition:leave="transition ease-in duration-200 transform"
+                                                    x-transition:leave-start="opacity-100" 
+                                                    x-transition:leave-end="opacity-0"
+                                                    class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"
+                                                ></div>
+                                
+                                                <div x-cloak
+                                                    x-transition:enter="transition ease-out duration-300 transform"
+                                                    x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" 
+                                                    x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                                                    x-transition:leave="transition ease-in duration-200 transform"
+                                                    x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" 
+                                                    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                                    class="inline-block w-full max-w-lg p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl lg:max-w-lg"
+                                                >
+                                                    <div class="flex items-center justify-between space-x-4 px-2">
+                                                        <h1 class="text-xl font-medium text-gray-800 ">Cancel Swap Schedule</h1>
+                                
+                                                        <button @click="modelOpen = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                            </svg>
+                                                        </button>
+                                                    </div>
+                                                    <div class="">
+                                                        <form action="">
+                                                            <div class="mt-4 w-full px-2">
+                                                                <label class="block text-left text-sm text-gray-700 capitalize white:text-gray-200">Remarks</label>
+                                                                <textarea name="shift_from_rd" type="date" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40" rows="5"></textarea>
+                                                            </div>
+                                                            <div class="flex justify-end mt-6 px-2">
+                                                                <button type="submit" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-red-500 rounded-2xl w-full white:bg-red-600 white:hover:bg-red-700 white:focus:bg-red-700 hover:bg-red-600 focus:outline-none focus:bg-red-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                                                                    Cancel
+                                                                </button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table
+                    {{-- <table class=" text-sm text-left text-gray-500 white:text-gray-400" width="200%">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                            <tr class="">
+                                <th scope="col" class="py-3 px-6" >
+                                    Employee Name
+                                </th>
+                                <th scope="col" class="py-3 px-6" >
+                                    Filed Date
+                                </th>
+                                <th scope="col" class="py-3 px-6" >
+                                    Shift From (RD)
+                                </th>
+                                <th scope="col" class="py-3 px-6" >
+                                    Shift To (Duty)
+                                </th>
+                                <th scope="col" class="py-3 px-6" >
+                                    Shift From (Duty)
+                                </th>
+                                <th scope="col" class="py-3 px-6" >
                                     Shift To (RD)
                                 </th>
                                 <th scope="col" class="py-3 px-6" width="10%" align="center">
@@ -106,7 +220,6 @@
                             </tr>
                         </thead>
                         <tbody>
-                           
                             @foreach($swapdata AS $sd)
                             <tr class="bg-white border-b white:bg-gray-800 white:border-gray-700 hover:bg-gray-50 white:hover:bg-gray-600">
                                 <td scope="row" class="py-3 px-6 font-medium text-gray-900 whitespace-nowrap white:text-white">
@@ -140,7 +253,7 @@
                             </tr>
                             @endforeach
                         </tbody>
-                    </table>
+                    </table> --}}
                 </div>
             </div>
             
