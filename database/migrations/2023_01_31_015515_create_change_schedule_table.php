fd4a11d9d0d2fc74159e818e4f7b05b6d66e7e2a
unknown <jonahbenares.cenpri@gmail.com>
2023-01-31 09:59:36 +0800

    change sched table

---
+++ database/migrations/2023_01_31_015515_create_change_schedule_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('change_schedule', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_applied')->nullable();
+            $table->foreignId('employee_id')->contrained('employees');
+            $table->integer('personal_id');
+            $table->string('schedule_code')->nullable();
+            $table->string('month_year')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('change_schedule');
+    }
+};
