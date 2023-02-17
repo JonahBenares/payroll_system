8cc7f942665afe1645767c157ba25461ced8ba81
unknown <jonahbenares.cenpri@gmail.com>
2023-02-17 14:03:23 +0800

    alter adj_calc_detail table

---
+++ database/migrations/2023_02_17_045437_add_columns_to_adj_calc_detail_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('adj_calc_detail', function (Blueprint $table) {
+            $table->renameColumn('rd_date', 'log_date');
+            $table->float('regular_amount',10,2)->default('0')->after('regular_hours');
+            $table->integer('rest_day')->default('0')->after('hourly_rate');
+            $table->integer('holiday')->default('0')->after('rd_amount');
+            $table->integer('night_premium')->default('0')->after('holiday_amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('adj_calc_detail', function (Blueprint $table) {
+            //
+        });
+    }
+};
