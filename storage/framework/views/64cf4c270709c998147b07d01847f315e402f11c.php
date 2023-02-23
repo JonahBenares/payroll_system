da34be8f1cdeb8ab4f70702b4a97a7e8bf9c03a2
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2023-02-23 15:22:38 +0800

    Datatable masterfile

---
+++ storage/framework/views/64cf4c270709c998147b07d01847f315e402f11c.php
@@ -0,0 +1,109 @@
+<?php if (isset($component)) { $__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da = $component; } ?>
+<?php $component = App\View\Components\AppLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(App\View\Components\AppLayout::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+     <?php $__env->slot('header', null, []); ?>  <?php $__env->endSlot(); ?>
+    <style>
+        [x-cloak] {
+        display: none;
+        }
+    </style>        
+    <div class="overflow-auto h-screen pb-28 pt-2 pr-2 pl-2 md:pt-0 md:pr-0 md:pl-0">
+        <div class="flex flex-col flex-wrap sm:flex-row">
+            <div class="relative h-full w-full text-center  bg-white rounded-lg shadow-lg white:bg-gray-800 white:border-gray-700">
+                <div class="flex justify-between pr-4 pl-4 pt-4 rounded-t-lg">
+                    <div > 
+                        <h2 class="uppercase font-semibold py-2">Adjustment Rates</h2>
+                    </div>
+                    <div class="flex">
+                        <a href="<?php echo e(route('adjustmentrate.create')); ?>" type="button" >
+                            <div class="flex items-center justify-center px-3 py-2 mx-2 space-x-2 text-sm tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-3xl white:bg-blue-600 white:hover:bg-blue-700 white:focus:bg-blue-700 hover:bg-blue-600 focus:outline-none focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+                                </svg>
+                                <span>Add New</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <?php if(Session::has('success')): ?>
+                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                        <span class="block sm:inline"><?php echo e(Session::get('success')); ?></span>
+                    </div>
+                <?php endif; ?>
+                <div class="overflow-x-auto overflow-y-hidden hover:overflow-y-auto h-100 relative p-4 rounded-b-lg">
+                    <table class="w-full text-sm text-left white:text-gray-400" id="table_overall">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 white:bg-gray-700 white:text-gray-400 sticky top-0 z-1">
+                            <tr class="">
+                                <th scope="col" class="py-3 px-6" width="30%">
+                                    Adjustment Rate name
+                                </th>
+                                <th scope="col" class="py-3 px-6" width="30%">
+                                    Deduction Type
+                                </th>
+                                <th scope="col" class="py-3 px-6" width="30%">
+                                    Rate
+                                </th>
+                                <th scope="col" class="py-3 px-6" width="10%" align="center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                    </svg>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $__currentLoopData = $adjustmentrate; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ar): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <tr class="bg-white text-gray-500 font-medium hover:bg-gray-50 hover:text-gray-900 hover:font-semibold">
+                                <td scope="row" class="py-3 px-6 whitespace-nowrap white:text-white">
+                                    <?php echo e($ar->description); ?>
+
+                                </td>
+                                <td class="py-4 px-6">
+                                    <?php echo e(($ar->deduction_type==1) ? 'Percentage' : 'Actual Amount'); ?>
+
+                                </td>
+                                <td class="py-4 px-6">
+                                    <?php echo e(($ar->deduction_type==1) ? $ar->rate_amount * 100 ."%" : number_format($ar->rate_amount,2)); ?>
+
+                                </td>
+                                <td class="py-4 px-6 flex justify-center" align="center">
+                                    <a href="<?php echo e(route('adjustmentrate.edit' , $ar->id)); ?>" class="" title="Update">
+                                        <div class="py-2 px-2 text-xs font-medium text-center text-white transition-colors bg-indigo-500 rounded-2xl white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                                <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                                                <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                                            </svg>
+                                        </div>
+                                    </a>
+                                    <form method="post" action="<?php echo e(route('adjustmentrate.destroy',$ar->id)); ?>" onsubmit="return confirm('Are you sure you want to delete this record?');">
+                                        <?php echo method_field('delete'); ?>
+                                        <?php echo csrf_field(); ?>
+                                        <button type="submit" class="ml-1 py-2 px-2 text-xs font-medium text-center text-white bg-red-500 rounded-2xl hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-blue-300 white:bg-blue-600 white:hover:bg-red-500 white:focus:ring-blue-800" title="Delete">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                            </svg>   
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </tbody>
+                    </table>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da)): ?>
+<?php $component = $__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da; ?>
+<?php unset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da); ?>
+<?php endif; ?>
+<?php /**PATH C:\xampp\htdocs\payroll_system\resources\views/adjustments/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8a1a8aca67107d5e11e7435f09f2a7e8f5707d5a.php
@@ -0,0 +1,115 @@
+<?php if (isset($component)) { $__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da = $component; } ?>
+<?php $component = App\View\Components\AppLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(App\View\Components\AppLayout::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+     <?php $__env->slot('header', null, []); ?>  <?php $__env->endSlot(); ?>
+    <!-- component -->
+    <style>
+        [x-cloak] {
+        display: none;
+        }
+    </style>
+    
+    <div class="overflow-auto h-screen pb-28 pt-2 pr-2 pl-2 md:pt-0 md:pr-0 md:pl-0">
+        <div class="flex flex-col flex-wrap sm:flex-row ">
+            <div class="relative h-full w-full text-center bg-white rounded-lg shadow-lg">
+                <div class="flex justify-between pr-4 pl-4 pt-4 rounded-t-lg ">
+                    <div > 
+                        <h2 class="uppercase font-semibold py-2">Statutory Bracket <small class="border-l-2 px-1">Update</small></h2>
+                    </div>
+                    <div class="flex">
+                        <a href= "<?php echo e(route('statutorybracket.index')); ?>" type="button">
+                            <div class="flex items-center justify-center px-3 py-2 mx-2 space-x-2 text-sm tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-3xl white:bg-blue-600 white:hover:bg-blue-700 white:focus:bg-blue-700 hover:bg-blue-600 focus:outline-none focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                <span>Show List</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="p-4">
+                    <?php if(Session::has('success')): ?>
+                        <div class="mb-5 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                            <span class="block sm:inline"><?php echo e(Session::get('success')); ?></span>
+                        </div>
+                    <?php endif; ?>
+                    <?php if(Session::has('fail')): ?>
+                        <div class="mb-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                            <span class="block sm:inline"><?php echo e(Session::get('fail')); ?></span>
+                        </div>
+                    <?php endif; ?>
+                    <form action="<?php echo e(route('statutorybracket.update',$statutory->id)); ?>" method='POST'>
+                    <?php echo csrf_field(); ?>
+
+                        <?php echo method_field("PATCH"); ?>
+                        <div class="">
+                            <label for="" class="block text-left text-sm text-gray-700 capitalize white:text-gray-200">Statutory Name</label>
+                            <!-- <input type="text" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40"> -->
+                            <select type="text" name="payslip_info_id" id="payslip_info_id" class="block w-full  px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            <option value="">Select Statutory Name</option>
+                            <?php $__currentLoopData = $payslip; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $p): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <option value="<?php echo e($p->id); ?>" <?php echo e(($p->id  == $statutory->payslip_info_id) ? 'selected' : ''); ?>><?php echo e($p->description); ?></option>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </select>
+                        </div>
+                        
+                        <div class="flex justify-between space-x-2 mt-4">
+                            <div class="w-full">
+                                <label class="block text-left text-sm text-gray-700 capitalize white:text-gray-200">Salary From</label>
+                                <input type="text" name="salary_from" id="salary_from" value="<?php echo e($statutory->salary_from); ?>" onkeypress="return isNumberKey(this, event)" class="text-sm block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            </div>
+                            <div class="w-full">
+                                <label class="block text-left text-sm text-gray-700 capitalize white:text-gray-200">Salary To</label>
+                                <input type="text" name="salary_to" id="salary_to" value="<?php echo e($statutory->salary_to); ?>" onkeypress="return isNumberKey(this, event)" class="text-sm block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            </div>
+                        </div>
+                        <div class="flex justify-between space-x-2 mt-4">
+                            <div class="w-full">
+                                <label for="" class="block text-left text-sm text-gray-700 capitalize white:text-gray-200">Deductions</label>
+                                <input type="text" name="deduction_amount" id="deduction_amount" value="<?php echo e($statutory->deduction_amount); ?>" onkeypress="return isNumberKey(this, event)" type="text" class="text-sm block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            </div>
+                            <div class="w-full">
+                                <label for="" class="block text-left text-sm text-gray-700 capitalize white:text-gray-200">As of Date</label>
+                                <input type="date" name="as_of" id="as_of" value="<?php echo e($statutory->as_of); ?>" class="text-sm block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            </div>
+                        </div>
+
+                        <div class="flex justify-end mt-6">
+                            <button type="submit" value="Save" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-2xl w-full white:bg-indigo-600 white:hover:bg-indigo-700 white:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                                Update
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+        </div>
+    </div>    
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da)): ?>
+<?php $component = $__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da; ?>
+<?php unset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da); ?>
+<?php endif; ?>
+<script>
+    function isNumberKey(txt, evt){
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode == 46) {
+            //Check if the text already contains the . character
+            if (txt.value.indexOf('.') === -1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (charCode > 31
+                && (charCode < 48 || charCode > 57))
+                return false;
+        }
+        return true;
+    }
+</script>
+<?php /**PATH C:\xampp\htdocs\payroll_system\resources\views/statutory/edit.blade.php ENDPATH**/ ?>
